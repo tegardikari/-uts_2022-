c83daf9f4c7dfe8babb22d3453872f7a6de9774c
tegardikari <111736182+tegardikari@users.noreply.github.com>
2022-09-19 20:18:42 +0700

    Add files via upload

---
+++ TEGAR DIKARI_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+  
+ using namespace std; 
+  
+ int main () { 
+    
+   string nama; 
+   string Umur; 
+   cout << "Nama : "; 
+       cin >> nama; 
+        
+       cout << "Umur : "; 
+       cin >> Umur; 
+   return 0; 
+ }
